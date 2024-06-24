d5b90cad0e39cbcbdccdba44058465b7cf6bcc0e
MarianoGitHubs <168311068+MarianoGitHubs@users.noreply.github.com>
2024-06-23 22:44:22 -0300

    arreglos

--- SliderTestimoniosFRONTEND-main/SliderTestimoniosFRONTEND/Responsive-Card-Slider-main/js/script.js
+++ SliderTestimoniosFRONTEND-main/SliderTestimoniosFRONTEND/Responsive-Card-Slider-main/js/script.js
@@ -1,29 +1,29 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  slidesPerView: 2.1, // Ajusta ligeramente para mostrar un poco más de la tercera diapositiva
+  spaceBetween: 20, // Ajusta el espacio entre las diapositivas
+  loop: true,
+  centerSlide: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 2.1, // Mantén 2.1 para las pantallas más grandes
+      spaceBetween: 20, // Ajusta el espacio entre las diapositivas
     },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
+    950: {
+      slidesPerView: 2.1, // Mantén 2.1 para las pantallas más grandes
+      spaceBetween: 20, // Ajusta el espacio entre las diapositivas
     },
-  });
+  },
+});
